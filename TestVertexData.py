47360de868b6c4b5215d6b2d089dc43ea19f10db
Hamiltonicity <john@lapinskas.co.uk>
2024-11-24 14:08:22 +0000

    Updated existing unit tests, added unit tests for lattice.

--- TestVertexData.py
+++ TestVertexData.py
@@ -7,6 +7,13 @@ class TestVertexData(unittest.TestCase):
         distance_function = (lambda x, y: 0.0 if x == y else 1.0)  # Unit metric
         self.test_instance = VertexData(dimension=2, distance_function=distance_function)
 
+    def test_properties(self):
+        point_dict = {57: (1., 1.), "Frog": (2., 2.), 11.5: (3., 3.)}
+        self.test_instance.setPointsFromIds(point_dict)
+
+        self.assertEqual(set(self.test_instance.positions), {(1., 1.), (2., 2.), (3., 3.)})
+        self.assertEqual(set(self.test_instance.ids), {57, "Frog", 11.5})
+
     def test_setpoints(self):
         point_list = [(1.,1.), (2.,2.), (3.,3.)]
         self.test_instance.setPoints(point_list)
@@ -63,6 +70,40 @@ class TestVertexData(unittest.TestCase):
         # Ball should be closed
         points_found = self.test_instance.getIdsInBall(center=(1., 1.), radius=1.0)
         self.assertEqual(set(points_found), {0,1,2})
+
+class TestLattice(unittest.TestCase):
+    def test_1d(self):
+        test_instance = lattice(dimension=1, size=6)
+        point_set = {(0.,), (1.,), (2.,), (3.,), (4.,), (5.,)}
+        self.assertEqual(set(test_instance.positions), point_set)
+
+        self.assertEqual(test_instance.distance((0.,), (1.,)), 1.)
+        self.assertEqual(test_instance.distance((0.,), (5.,)), 1.)
+        self.assertEqual(test_instance.distance((0.,), (4.,)), 2.)
+
+        point_ids = test_instance.getIdsInAnnulus((0.,0.), 1.5, 3.5)
+        positions = {test_instance.id_to_position[point_id] for point_id in point_ids}
+        self.assertEqual(positions, {(2.,), (3.,), (4.,)})
+
+
+    def test_2d(self):
+        test_instance = lattice(dimension=2,size=4)
+        point_set = {(0.,0.), (0.,1.), (0.,2.), (0.,3.),
+                     (1.,0.), (1.,1.), (1.,2.), (1.,3.),
+                     (2.,0.), (2.,1.), (2.,2.), (2.,3.),
+                     (3.,0.), (3.,1.), (3.,2.), (3.,3.)}
+        self.assertEqual(set(test_instance.positions), point_set)
+
+        self.assertEqual(test_instance.distance((0., 0.), (1., 0.)), 1.)
+        self.assertEqual(test_instance.distance((0., 0.), (0., 1.)), 1.)
+        self.assertEqual(test_instance.distance((0., 0.), (3., 0.)), 1.)
+        self.assertEqual(test_instance.distance((0., 0.), (0., 3.)), 1.)
+        self.assertEqual(test_instance.distance((0., 0.), (0., 2.)), 2.)
+        self.assertEqual(test_instance.distance((0., 0.), (3., 3.)), pow(2,0.5))
+
+        point_ids = test_instance.getIdsInAnnulus((0.,3.), 1.1, 2.1)
+        positions = {test_instance.id_to_position[point_id] for point_id in point_ids}
+        self.assertEqual(positions, {(0., 1.), (1., 0), (1., 2.), (2., 3.), (3., 0.), (3., 2.)})
 
 if __name__ == '__main__':
     unittest.main()
