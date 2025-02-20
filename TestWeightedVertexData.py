084b6571e6f236ff631d5dc37f43d39a9b2314db
Hamiltonicity <john@lapinskas.co.uk>
2025-02-20 14:46:26 +0000

    Added new WeightedVertexData class, with presets for i.i.d. power law weights and for Gowalla-style weights-from-degrees.

---
+++ TestWeightedVertexData.py
@@ -0,0 +1,111 @@
+import unittest
+from TestDistribution import dkw_p_value
+from WeightedVertexData import *
+from VertexData import VertexData, euclideanDistanceFunction
+from collections import defaultdict
+from math import floor
+
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        self.weights = {1: 1., 2: 2.5, "abc": 3.}
+        self.generator = fixed_weights_generator(self.weights)
+
+        self.vertices = VertexData(dimension=2, metric=euclideanDistanceFunction(d=2))
+        points = {1: (0, 0), 2: (2, 0), "abc": (2, 2)}
+        self.vertices.setPointsFromIds(points)
+
+    def testFromDegrees(self):
+        output = self.generator(self.vertices)
+        self.assertEqual(self.weights[1], output[1])
+        self.assertEqual(self.weights[2], output[2])
+        self.assertEqual(self.weights["abc"], output["abc"])
+
+    def testWeightedVertexData(self):
+        mu = 2.
+        zeta = 3.
+        data = WeightedVertexData(self.vertices, self.generator, mu=mu, zeta=zeta)
+
+        self.assertEqual(self.weights[1], data.weight(1))
+        self.assertEqual(self.weights[2], data.weight(2))
+        self.assertEqual(self.weights["abc"], data.weight("abc"))
+
+        data.resample_weights()
+        self.assertEqual(self.weights[1], data.weight(1))
+        self.assertEqual(self.weights[2], data.weight(2))
+        self.assertEqual(self.weights["abc"], data.weight("abc"))
+
+        self.assertEqual(2.5 ** mu * 2 ** zeta, data.penalty(1, 2))
+        self.assertEqual(2.5 ** mu * 3 ** mu * 2 ** zeta, data.penalty(2, "abc"))
+        self.assertAlmostEqual(3.0 ** mu * 8 ** (zeta/2), data.penalty(1, "abc"), delta=1e-13)
+
+    def testResample(self):
+        """Checks that resample_weights is actually resampling every vertex weight."""
+        entropy = 331375102187953107209086426124205679010
+        generator = np.random.default_rng(seed=entropy)
+
+        def weight_generator(vertices: VertexData) -> Dict[Any, float]:
+            return {id_: generator.random() for id_ in vertices.ids}
+
+        data = WeightedVertexData(self.vertices, weight_generator, mu=0., zeta=0.)
+        first_sample = {id_: data.weights[id_] for id_ in self.vertices.ids}
+        data.resample_weights()
+        second_sample = {id_: data.weights[id_] for id_ in self.vertices.ids}
+
+        self.assertEqual(first_sample.keys(), second_sample.keys())
+        for id_ in first_sample.keys():
+            self.assertNotEqual(first_sample[id_], second_sample[id_])
+
+
+class TestFromDegrees(unittest.TestCase):
+    def testFromDegrees(self):
+        degrees = {1: 40, 2: 20, "abc": 60, "def": 0}  # Average degree 30, penalty should be 20.
+        generator = from_degrees_generator(degrees)
+
+        vertices = VertexData(dimension=2, metric=euclideanDistanceFunction(d=2))
+        points = {1: (0, 0), 2: (1, 1), "abc": (2, 2), "def": (3, 3)}
+        vertices.setPointsFromIds(points)
+
+        output = generator(vertices)
+        self.assertEqual(20., output[1])
+        self.assertEqual(1., output[2])
+        self.assertEqual(40., output["abc"])
+        self.assertEqual(1., output["def"])
+
+
+class TestPowerLaw(unittest.TestCase):
+    def testPowerLaw(self):
+        """Runs a DKW-based test to check that the weight distribution in a 100k-vertex set with tau=2.5 and
+        scaling(w) = 2 is roughly what it should be, dividing into bins by taking the floor of each weight. The allowed
+        error in total variation distance is .01."""
+        entropy = 127743512994918990291592040963354975738  # Generated from numpy via SeedSequence().entropy
+        generator = np.random.default_rng(seed=entropy)
+
+        scaling = lambda w: 2*w
+        tau = 2.5
+        weight_generator = power_law_generator(tau=tau, ell=scaling, generator=generator)
+
+        n = 100000
+        vertices = VertexData(dimension=1, metric=euclideanDistanceFunction(d=1))
+        vertices.setPoints([(i,) for i in range(n)])
+        test_instance = WeightedVertexData(vertices=vertices, weight_generator=weight_generator, mu=0.0, zeta=0.0)
+
+        weight_counts = defaultdict(lambda: 0)
+        for i in vertices.ids:
+            weight_counts[floor(test_instance.weight(i))] += 1
+
+        r"""Pr(floor(2*W) = i) = Pr(W <= (i+1)/2) - Pr(W <= i/2) = (2/i)^{\tau-1} - (2/(i+1))^{\tau-1}."""
+        def target_pmf(i):
+            if i <= 1:
+                return 0
+            return (i/2) ** (1 - tau) - ((i+1)/2) ** (1 - tau)
+
+        # Check for stupid calculation errors
+        self.assertAlmostEqual(sum([target_pmf(i) for i in range(100000)]), 1, delta=0.01)
+
+        p_value = dkw_p_value(sample_data=weight_counts, pmf=target_pmf, tvd_bound=0.01)
+        self.assertEqual(2.291165156475741e-07, p_value)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ WeightedVertexData.py
@@ -0,0 +1,99 @@
+from VertexData import VertexData
+from typing import Any, Callable, Dict, Optional
+import girg_sampling.girgs as gs
+import numpy as np
+
+
+class WeightedVertexData:
+    def __init__(self, vertices: VertexData, weight_generator: Callable[[VertexData], Dict[Any, float]], mu: float,
+                 zeta: float):
+        """Stores a vertex set for a GIRG with both spatial and weight data. Vertices should be the underlying vertex
+        set. Weight_generator should be a function that (probably randomly) resamples weights for the given
+        VertexData, returning a dictionary from vertex IDs to weights. Mu should be the weight penalty,
+        and zeta should be the spatial penalty."""
+        self.vertices = vertices
+        self.weight_generator = weight_generator
+        self.weights = None
+        self.mu = mu
+        self.zeta = zeta
+        self.resample_weights()
+
+    def weight(self, id_: Any) -> float:
+        """Returns the weight of the vertex with the given id."""
+        return self.weights[id_]
+
+    def resample_weights(self) -> None:
+        """Resamples the vertex weights from the given generator function."""
+        self.weights = self.weight_generator(self.vertices)
+
+    def penalty(self, x_id, y_id) -> float:
+        """Returns the total penalty for a possible edge (specified by vertex IDs), not including the random cost."""
+        spatial_penalty = self.vertices.distance(x_id, y_id) ** self.zeta
+        weight_penalty = (self.weight(x_id) * self.weight(y_id)) ** self.mu
+        return spatial_penalty * weight_penalty
+
+
+def power_law_generator(tau: float, ell: Callable[[float], float] = id,
+                        generator: Optional[np.random.Generator] = None) -> Callable[[VertexData], Dict[Any, float]]:
+    """Returns a weight sampling function for WeightedVertexData which samples weights W i.i.d. from a power law, taking
+    Pr(W >= x) = 1 / x^{\tau - 1} and using the specified RNG, then applies the given scaling map to each weight."""
+    if tau <= 2:
+        raise ValueError("tau must be greater than 2 for the expected degrees to be finite.")
+    if generator is None:
+        generator = np.random.default_rng()
+
+    return lambda vertices: _power_law_sample(vertices=vertices, tau=tau, scaling=ell, generator=generator)
+
+
+def _power_law_sample(vertices: VertexData, tau: float, scaling: Callable[[float], float],
+                      generator: np.random.Generator) -> Dict[Any, float]:
+    """Samples weights W for the given VertexData i.i.d. from a power law, taking Pr(W >= x) = 1 / x^{\tau - 1}
+    and using the specified RNG, then applies the given scaling map to each weight."""
+
+    # The upper bound isn't included in the range, so this samples a 31-bit integer to pass to Cython.
+    C_seed = generator.integers(low=0, high=2**31)
+    weights = gs.generateWeights(n=vertices.size, ple=tau, seed=C_seed)
+    if scaling is not id:
+        efficient_ell = np.vectorize(scaling)
+        weights = efficient_ell(weights)
+
+    # We map an arbitrary weight to each vertex ID since they're all i.i.d. anyway.
+    weight_array = np.asarray(weights)
+    return dict(zip(vertices.ids, weight_array))
+
+
+def fixed_weights_generator(weights: Dict[Any, float]) -> Callable[[VertexData], Dict[Any, float]]:
+    """Returns a weight 'sampling function' for WeightedVertexData which just returns the given dictionary of
+    weights."""
+    return lambda vertices: weights
+
+
+def from_degrees_generator(degrees: Dict[Any, int]) -> Callable[[VertexData], Dict[Any, float]]:
+    """Returns a weight sampling function for WeightedVertexData which 'samples' weights by estimating them based on
+    a supplied dictionary mapping vertex IDs to vertex degrees in the base graph. Used for the Gowalla dataset.
+
+    IMPORTANT: We really don't understand why this seems to work. Do not submit the paper before we do. A more
+    principled approach is likely going to need our estimated value of alpha. John thinks the ideologically correct
+    form of this is probably going to map a degree d to max(1, d/C) for some C depending on alpha and the average
+    degree, as a result of correcting for three sources of error:
+
+    1. The true degree distribution is going to have a constant scaling factor, i.e. Pr(d(v) >= x) ~ C_1 / x^{\tau-1}
+    for some constant C_1. We can estimate C_1 by comparing the average degree to the expected average degree with
+    \int_1^\infty 1/x^{\tau-1} = 1 / (\tau - 2).
+    2. The expected degrees aren't the same as the weights. This is probably roughly E(d(v)) ~ C_2 * W_v for some
+    constant factor C_2 depending on \alpha - need to take a look at Johannes's old calculations. IIRC there's no nice
+    closed form for the integral but we can at least do a numerical approximation even if it turns out not to be
+    well-approximated by a constant factor.
+    3. You can't have a weight smaller than 1, so every vertex of degree less than a certain threshold should be
+    considered to have weight 1.
+
+    By themselves, 1. and 2. wouldn't affect the relative penalty values at all - vertices would still be infected in
+    the same order - but in conjunction with 3 they become significant."""
+    average_degree = sum(degrees.values()) / len(degrees)
+    penalty = 2 * average_degree / 3
+
+    def weight_estimate(id_: Any) -> float:
+        return max(1., degrees[id_] - penalty)
+
+    weights = {id_: weight_estimate(id_) for id_ in degrees.keys()}
+    return fixed_weights_generator(weights)
