49a05dc40e7591ceb305d9f52f4adb06b55665a6
Hamiltonicity <john@lapinskas.co.uk>
2025-07-01 00:30:45 +0100

    Hill's estimator unit test.

--- tests/TestParameterEstimates.py
+++ tests/TestParameterEstimates.py
@@ -1,7 +1,10 @@
+from math import ceil
 import unittest
 
+import numpy as np
+
 from PresetGraph import GraphData
-from figures.ParameterEstimates import get_low_degree_edges, get_degree_sequence
+from figures.ParameterEstimates import get_low_degree_edges, get_degree_sequence, get_hill_coefficients
 from VertexSet import VertexSet, EuclideanDistance
 
 
@@ -31,8 +34,19 @@ class TestLowDegree(unittest.TestCase):
 
 class TestHillCoefficients(unittest.TestCase):
     def test(self):
-        """TODO: Give it a sample from actual noisy power law data with a set seed."""
-        pass
+        self.seed = 274286783427111506408736539148187402179
+        rng = np.random.default_rng(self.seed)
+        # This is cooked up to be a distribution Hill's estimator should thrive on - large integer values so adding
+        # uniform noise has a similar effect to a degree distribution, and Pr(X >= x) ~ x^(-1.5) so we should expect
+        # tau roughly equal to 2.5.
+        sample_data = [ceil(100/rng.power(1.5)) for _ in range(10000)]
+        sample_data.sort(reverse=True)
+
+        hill_data = get_hill_coefficients(np.array(sample_data))
+        self.assertEqual(hill_data.kappa, 9670)
+        self.assertEqual(hill_data.xi, 0.6625810969768464)
+        self.assertEqual(hill_data.tau, 2.509249214266287)
+        print(hill_data)
 
 
 class TestAlphaRegression(unittest.TestCase):
